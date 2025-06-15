044145705d46cb71ad442f9930a30a6762c5083e
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-06-15 15:30:23 +0530

    Add Customer entity with JPA annotations and Lombok support

---
+++ src/main/java/com/customermanager/model/Customer.java
@@ -0,0 +1,38 @@
+package com.customermanager.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "Customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "Customer_id")
+    private int id;
+
+    @Column(name = "First_Name", nullable = false)
+    private String firstName;
+
+    @Column(name = "Last_Name", nullable = false)
+    private String lastName;
+
+    @Column(name = "City", nullable = false)
+    private String city;
+
+    @Builder
+    public Customer(String firstName, String lastName, String city) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+    }
+}
