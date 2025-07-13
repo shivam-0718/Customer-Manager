2bc3d288d9ed7d4723479684455fe602a0d3269f
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-07-13 13:31:27 +0530

    Added Swagger API annotations.

--- src/main/java/com/customermanager/CustomerManagerApplication.java
+++ src/main/java/com/customermanager/CustomerManagerApplication.java
@@ -1,9 +1,24 @@
 package com.customermanager;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Customer Manager Application",
+				version = "1.0",
+				description = "This application will ." +
+						"manage customer information, including registration, updating, and deletion of customer records."
+		),
+		servers = @Server(
+				url = "http://localhost:8084/customer-manager",
+				description = "This application is available in the following URL location."
+		)
+)
 public class CustomerManagerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/customermanager/web/CustomerController.java
+++ src/main/java/com/customermanager/web/CustomerController.java
@@ -2,6 +2,8 @@ package com.customermanager.web;
 
 import com.customermanager.model.Customer;
 import com.customermanager.service.ICustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
+@Tag(name = "Customer Controller", description = "Handles customer data, registration, updates, and deletion.")
 public class CustomerController {
     private ICustomerService service;
 
@@ -21,6 +24,7 @@ public class CustomerController {
     }
 
     @GetMapping("/customer-info")
+    @Operation(summary = "GET Operation", description = "This method is used to retrieve the customer details and display them on the customer info page.")
     public String getCustomerData(Model model) {
         System.out.println("Fetching customer data..."); //for debugging purposes
         List<Customer> customers = service.getCustomerInfo();
@@ -30,6 +34,7 @@ public class CustomerController {
     }
 
     @PostMapping("/register-customer")
+    @Operation(summary = "POST Operation", description = "This method is used to register the customer details and display the updated customer info list on the customer info page.")
     public String registerCustomer(@ModelAttribute("customer") Customer customer, Model model) {
         System.out.println("Registering customer: " + customer); //for debugging purposes
         service.registerCustomer(customer);
@@ -37,12 +42,14 @@ public class CustomerController {
     }
 
     @GetMapping("/show-form")
+    @Operation(summary = "GET Operation", description = "This method displays the Customer registration form.")
     public String showForm(Model model) {
         model.addAttribute("customer", new Customer()); // Create a new Customer object to bind form data
         return "showform";
     }
 
     @GetMapping("/update-form")
+    @Operation(summary = "GET Operation", description = "This method updates the existing Customer data with the help of UI.")
     public String updateCustomerInfo(@RequestParam("customerId") Integer id, Model model) {
         Customer cx = service.fetchCustomerById(id);
         model.addAttribute("customer", cx);
@@ -50,6 +57,7 @@ public class CustomerController {
     }
 
     @GetMapping("/delete-data")
+    @Operation(summary = "GET Operation", description = "This method deletes the existing Customer data with the help of UI.")
     public String deleteCustomerInfo(@RequestParam("customerId") Integer id) {
         service.deleteCustomer(id);
         return "redirect:/customer-info";
